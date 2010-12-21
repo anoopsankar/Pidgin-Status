77f61390529af8955e432caa013bd628a81c117a
Anoop Sankar <anoop@mobme.in>
2010-12-21 11:11:45 +0530

    Added weather support to status.

--- pidgin_status.py
+++ pidgin_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys, dbus, datetime
+import weather
 
 sessionBus = dbus.SessionBus()
 purpleObj = sessionBus.get_object("im.pidgin.purple.PurpleService", "/im/pidgin/purple/PurpleObject")
@@ -13,11 +14,34 @@ def pidgin_status(message):
 	purple.PurpleSavedstatusSetMessage(new_status, message)
 	purple.PurpleSavedstatusActivate(new_status)
 
-date1 = datetime.datetime(2010,3,1,8,30,00)
-date2 = datetime.datetime.now()
-ddiff = date2 - date1
+def format_status():
+	symbolLookup = {
+	    'clear':u'\u263C',
+	    'partly sunny':u'\u2600',
+	    'scattered thunderstorms':u'\u2602',
+	    'showers':u'\u2602',
+	    'scattered showers':u'\u2602',
+	    'overcast':u'\u2601',
+	    'chance of rain':u'\u2601',
+	    'sunny':u'\u2600',
+	    'mostly sunny':u'\u2600',
+	    'partly cloudy':u'\u2601',
+	    'mostly cloudy':u'\u2601',
+	    'chance of storm':u'\u2601',
+	    'rain':u'\u2602',
+	    'chance of snow':u'\u2603',
+	    'cloudy':u'\u2601',
+	    'mist':u'\u2601',
+	    'storm':u'\u2601',
+	    'thunderstorm':u'\u2602',
+	    'chance of tstorm':u'\u2602',
+	    'light rain':u'\u2602'
+	}
 
-pidgin_status(str(ddiff.days) + ":" + str(ddiff.seconds / 3600))
+	weatherString = weather.getWeather("Kochi")
+	return symbolLookup[weatherString.lower()]
 
-#Log
-print "Status changed at " + str(datetime.datetime.now())
+status = format_status()
+pidgin_status(status)
+
+print status
